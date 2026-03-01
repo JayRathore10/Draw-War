cd385021e3e18a26d66d7d828fbe6c346f7217da
Jay <jayrathore88155@gmail.com>
2026-03-01 13:59:36 +0530

    Add snapshot of shape

--- client/src/components/DrawBoard.tsx
+++ client/src/components/DrawBoard.tsx
@@ -1,107 +1,108 @@
-import React, { useEffect, useRef , useState} from "react";
+import React, { useEffect, useRef, useState } from "react";
 
-const DrawBoard : React.FC = ()=>{
+const DrawBoard: React.FC = () => {
   const canvasRef = useRef<HTMLCanvasElement | null>(null);
-  const [isDrawing , setIsDrawing] = useState(false);
-  const [mode , setMode] = useState<"pencil" | "rectangle" | "circle">("pencil");
-  const [startPos , setStartPos] = useState<{x : number , y : number} | null> (null); 
+  const [isDrawing, setIsDrawing] = useState(false);
+  const [mode, setMode] = useState<"pencil" | "rectangle" | "circle">("pencil");
+  const [startPos, setStartPos] = useState<{ x: number, y: number } | null>(null);
+  const snapshotRef = useRef<ImageData | null>(null);
 
-  useEffect(()=>{
-    const canvas = canvasRef.current ;
-    if(!canvas) return ;
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    if (!canvas) return;
 
     const ctx = canvas.getContext("2d");
-    if(!ctx) return ;
+    if (!ctx) return;
 
     ctx.lineWidth = 3;
     ctx.lineCap = "round";
     ctx.strokeStyle = "white";
-  } , []);
+  }, []);
 
-  const startDrawing = (e : React.MouseEvent<HTMLCanvasElement>)=>{
-    const ctx = canvasRef.current?.getContext("2d");
-    if(!ctx) return ;
+  const startDrawing = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext("2d");
+    if (!canvas || !ctx) return;
 
     const x = e.nativeEvent.offsetX;
     const y = e.nativeEvent.offsetY;
 
-    if(mode === "pencil"){
-      ctx.beginPath();
-      ctx.moveTo(x , y);
-    }
-    setStartPos({x , y});
+    setStartPos({ x, y });
     setIsDrawing(true);
+
+    if (mode === "pencil") {
+      ctx.beginPath();
+      ctx.moveTo(x, y);
+    } else {
+      snapshotRef.current = ctx.getImageData(0, 0, canvas.width, canvas.height);
+    }
   }
 
-  const draw = (e: React.MouseEvent<HTMLCanvasElement>) =>{
-    if(!isDrawing || !startPos) return ;
-    const ctx = canvasRef.current?.getContext("2d");
-    if(!ctx) return;
+  const draw = (e: React.MouseEvent<HTMLCanvasElement>) => {
+    if (!isDrawing || !startPos) return;
+
+    const canvas = canvasRef.current;
+    const ctx = canvas?.getContext("2d");
+    if (!canvas || !ctx) return;
 
     const x = e.nativeEvent.offsetX;
     const y = e.nativeEvent.offsetY;
 
-    if(mode === "pencil"){
-      ctx.lineTo(x , y);
+    if (mode === "pencil") {
+      ctx.lineTo(x, y);
+      ctx.stroke();
+    } else {
+      if (snapshotRef.current) {
+        ctx.putImageData(snapshotRef.current, 0, 0);
+      }
+      ctx.beginPath();
+
+      if (mode == "rectangle") {
+        ctx.rect(
+          startPos.x,
+          startPos.y,
+          x - startPos.y,
+          y - startPos.y
+        );
+      }
+      if (mode === "circle") {
+        const radius = Math.sqrt(
+          Math.pow(x - startPos.x, 2) +
+          Math.pow(y - startPos.y, 2)
+        );
+
+        ctx.arc(startPos.x, startPos.y, radius, 0, 2 * Math.PI);
+      }
+
       ctx.stroke();
     }
   }
 
-  const stopDrawing = (e : React.MouseEvent<HTMLCanvasElement>)=>{
-    if(!isDrawing || !startPos) return ;
-
-    const ctx = canvasRef.current?.getContext("2d");
-    if(!ctx) return ;
-
-    const endX = e.nativeEvent.offsetX;
-    const endY = e.nativeEvent.offsetY;
-
-    if(mode === "rectangle"){
-      ctx.beginPath();
-      ctx.rect(
-        startPos.x  , 
-        startPos.y , 
-        endX - startPos.x ,
-        endY - startPos.y
-      )
-      ctx.stroke();
-    }
-
-    if(mode === "circle"){
-      const radius = Math.sqrt(
-        Math.pow(endX - startPos.x , 2) + 
-        Math.pow(endY - startPos.y , 2)
-      );
-
-      ctx.beginPath();
-      ctx.arc(startPos.x , startPos.y ,radius , 0 , 2 * Math.PI);
-      ctx.stroke();
-    }
-
+  const stopDrawing = () => {
     setIsDrawing(false);
     setStartPos(null);
-  }
+  };
 
-  return(
+  return (
     <>
 
-      <div style={{marginBottom : "10px"}}>
+      <div style={{ marginBottom: "10px" }}>
         <button
-          onClick={()=> setMode("pencil")}
+          onClick={() => setMode("pencil")}
         >pencil</button>
         <button
-          onClick={()=> setMode("rectangle")}
+          onClick={() => setMode("rectangle")}
         >Rectangle</button>
         <button
-          onClick={()=> setMode("circle")}
+          onClick={() => setMode("circle")}
         >Circle</button>
       </div>
 
-      <canvas 
-        ref = {canvasRef}
+      <canvas
+        ref={canvasRef}
         width={800}
         height={500}
-        style ={{border : "2px solid black"}}
+        style={{ border: "2px solid black" }}
         onMouseDown={startDrawing}
         onMouseMove={draw}
         onMouseUp={stopDrawing}
