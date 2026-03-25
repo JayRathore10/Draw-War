bc6db07ed1d1eb0358d6d1d8f24f65e7f77928a1
Jay <jayrathore88155@gmail.com>
2026-03-25 20:57:07 +0530

    Minor fix in game.socket.ts

--- server/src/socket/game.socket.ts
+++ server/src/socket/game.socket.ts
@@ -1,12 +1,12 @@
 import { Server, Socket } from "socket.io";
 
 export const setupSockets = (io: Server) => {
-  // matchMatching varibale 
-  // to understand player is waiting or not
   let waitingPlayer: Socket | null = null;
 
   io.on("connection", (socket: Socket) => {
     console.log("User connected:", socket.id);
+
+    socket.emit("connected", { socketId: socket.id });
 
     if (waitingPlayer) {
       const roomId = `${waitingPlayer.id}#${socket.id}`;
@@ -22,16 +22,29 @@ export const setupSockets = (io: Server) => {
       waitingPlayer = null;
     } else {
       waitingPlayer = socket;
+      socket.emit("waiting", { message: "Waiting for opponent..." });
     }
 
     socket.on("draw", ({ roomId, data }) => {
       socket.to(roomId).emit("opponent-draw", data);
     });
 
+    socket.on("clear", (roomId: string) => {
+      socket.to(roomId).emit("opponent-clear");
+    });
+
     socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+
       if (waitingPlayer?.id === socket.id) {
         waitingPlayer = null;
       }
+
+      socket.rooms.forEach((roomId) => {
+        if (roomId !== socket.id) {
+          socket.to(roomId).emit("opponent-left");
+        }
+      });
     });
   });
 };
