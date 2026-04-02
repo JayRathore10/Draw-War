d31d16f9a3ed2796424e079756beb19fdfee66e5
Jay <jayrathore88155@gmail.com>
2026-04-02 22:55:15 +0530

    Add function to save images

--- server/src/controllers/image.controller.ts
+++ server/src/controllers/image.controller.ts
@@ -19,10 +19,35 @@ export const compareImages = async(req : Request, res : Response , next : NextFu
       });
     }
 
-    res.json({
-      success : true 
-    })
+    const uploadDir = path.join(__dirname , "uploads");
 
+    if(!fs.existsSync(uploadDir)){
+      fs.mkdirSync(uploadDir);
+    }
+
+    const base64ToBuffer = (base64 : string)=>{
+      const data = base64.replace("/^data:image\/png;base64,/" , "");
+      return Buffer.from(data , "base64");
+    };
+
+    const player1Buffer = base64ToBuffer(player1Base64);
+    const player2Buffer = base64ToBuffer(player2Base64);
+
+    const player1Path = path.join(uploadDir , player1ImageName);
+    const player2Path = path.join(uploadDir , player2ImageName);
+
+    fs.writeFileSync(player1Path  , player1Buffer);
+    fs.writeFileSync(player2Path , player2Buffer);
+
+    console.log("Saved:" , player1ImageName , player2ImageName);
+
+    res.json({
+      message : "Images saved successfully" , 
+      files : {
+        player1: player1ImageName, 
+        player2 : player2ImageName
+      }
+    });
   }catch(error){
     next(error);
   }
